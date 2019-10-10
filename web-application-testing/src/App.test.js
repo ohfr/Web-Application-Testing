392a10b0840853094cc9db922371a2cf47766645
Daniel <danielmartin1205@gmail.com>
2019-10-10 18:54:37 -0400

    added tests, needs stlying

--- web-application-testing/src/App.test.js
+++ web-application-testing/src/App.test.js
@@ -6,3 +6,35 @@ import App from './App';
 
 
 afterEach(rtl.cleanup);
+
+
+test('Strike to be 0 or more', () => {
+    const wrapper = rtl.render(<App />);
+    const element = wrapper.queryByRole(/strike/i);
+    expect(parseInt(element.innerHTML)).toBeGreaterThanOrEqual(0);
+})
+
+test('Balls to be 0 or more', () => {
+    const wrapper = rtl.render(<App />);
+    const element = wrapper.queryByRole(/ball/i);
+    expect(parseInt(element.innerHTML)).toBeGreaterThanOrEqual(0);
+})
+
+test('Strikes to be no more than 3', () => {
+    const wrapper=rtl.render(<App />);
+    const element = wrapper.queryByRole(/strike/i);
+    expect(parseInt(element.innerHTML)).toBeLessThanOrEqual(3);
+})
+
+test('Balls to be equal or less than 4', () => {
+    const wrapper =rtl.render(<App />);
+    const element = wrapper.queryByRole(/ball/i);
+    expect(parseInt(element.innerHTML)).toBeLessThanOrEqual(4);
+})
+
+
+
+
+
+
+
--- web-application-testing/src/Components/Display.js
+++ web-application-testing/src/Components/Display.js
@@ -6,7 +6,7 @@ const Display = (props) => {
     return (
         <div>
             <h2 role="strike" value={props.strikes}>Strikes: {props.strikes}</h2>
-            <h2>Balls: {props.balls} </h2>
+            <h2 role="balls">{props.balls} </h2>
         </div>
     )
 }
